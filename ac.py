32034f623fbdb7ddca2ec74918ec577f1311bdaa
fireyyouth <fireyyouth@gmail.com>
2018-10-24 03:20:00 +0800

    add green link, add exec code

--- ac.py
+++ ac.py
@@ -1,4 +1,6 @@
 from queue import Queue
+import sys
+
 class Node:
     def __init__(self):
         self.kids = {}
@@ -21,7 +23,7 @@ def build_tree(words):
         kid.blue = root
         q.put(kid)
     while not q.empty():
-        node = q.get() # blue link of this node is already set
+        node = q.get() # blue and green link of this node is already set
         assert node.blue != None
         for ch in node.kids:
             x = node.blue
@@ -31,6 +33,10 @@ def build_tree(words):
                 x = x.blue
             if ch in x.kids:
                 node.kids[ch].blue = x.kids[ch]
+                if x.kids[ch].flag:
+                    node.kids[ch].green = x.kids[ch]
+                elif x.kids[ch].green != None:
+                    node.kids[ch].green = x.kids[ch].green
             else:
                 node.kids[ch].blue = root
             q.put(node.kids[ch])
@@ -42,6 +48,8 @@ def print_tree_edges(node):
         print('%s [color=red];' % id(node))
     if node.blue != None:
         print('%s -> %s [color=blue];' % (id(node), id(node.blue)))
+    if node.green != None:
+        print('%s -> %s [color=green];' % (id(node), id(node.green)))
     for ch in node.kids:
         print('%s -> %s [color=black,label=%s];' % (id(node), id(node.kids[ch]), ch))
         print_tree_edges(node.kids[ch])
@@ -51,6 +59,41 @@ def print_tree(root):
     print_tree_edges(root)
     print('}')
 
-words = ['a','ab','bab','bc','bca','c','caa', 'cd']
+def run_tree(root, s):
+    res = []
+    node = root
+    for i in range(len(s)):
+        ch = s[i]
+
+        while ch not in node.kids:
+            if node is root:
+                break
+            node = node.blue
+
+        if ch in node.kids:
+            node = node.kids[ch]
+
+        x = node
+        while True:
+            if x.flag:
+                res.append((id(x), i))
+            if not x.green:
+                break
+            x = x.green
+
+    return res
+
+words = ['a','ab','bab','bc','bca','c','caa']
 tree = build_tree(words)
-print_tree(tree)
+
+cmd = 'draw'
+try:
+    cmd = sys.argv[1]
+except:
+    pass
+
+if cmd == 'draw':
+    print_tree(tree)
+elif cmd == 'test':
+    res = run_tree(tree, input())
+    print(res)
